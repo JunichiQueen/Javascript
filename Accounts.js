2022c9e9d1411ec5a661b2fe2956cef0674c3935
JunichiQueen <junichim@hotmail.com>
2019-06-12 15:09:52 +0100

    use of promises

--- Accounts.js
+++ Accounts.js
@@ -1,78 +1,114 @@
 
-let accName = document.getElementById("accName").value;
-let firstName = document.getElementById("firstName").value;
-let lastName = document.getElementById("lastName").value;
 
-function makeObject(accName, firstName, lastName){
+
+function makeObject(accName, firstName, lastName) {
     let object1 = {};
-    object1.accountNumber=accName;
-    object1.firstName=firstName;
-    object1.lastName=lastName;
-    return object1;
+    object1.accountNumber = accName;
+    object1.firstName = firstName;
+    object1.lastName = lastName;
+    let object2 = JSON.stringify(object1);
+    return object2;
 }
 
+function example(command, callback) {
+    command();
+    callback();
+}
 
-function getAnAccount() {
-    let req = new XMLHttpRequest();
+function getAllAccounts() {
 
-    let x = Number(document.getElementById("num1").value);
-
-    req.onload = function () {
+    makeRequest("GET", "http://localhost:8080/AccountSETemplate/api/account/getAllAccounts/").then(req => {
         let test = document.createElement("p");
         test.innerText = req.responseText;
         document.getElementById("display").appendChild(test);
-    }
-    req.open("GET", "http://localhost:8080/AccountSETemplate/api/account/getAnAccount/" + x);
-    req.send();
+    });
+  
+}
+
+function getAnAccount() {
+
+    let x = Number(document.getElementById("num1").value);
+    makeRequest("GET", "http://localhost:8080/AccountSETemplate/api/account/getAnAccount/" + x).then(req => {
+        let test = document.createElement("p");
+        test.innerText = req.responseText;
+        document.getElementById("display").appendChild(test);
+    });
+
 }
 
 
 function addAnAccount() {
-    let req = new XMLHttpRequest();
     
+    let accName = document.getElementById("accName").value;
+    let firstName = document.getElementById("firstName").value;
+    let lastName = document.getElementById("lastName").value;
+   
     let newObject = makeObject(accName, firstName, lastName);
-    let newJSON = JSON.stringify(newObject);
-    
-    req.onload = function () {
+
+    console.log(newObject);
+    makeRequest("POST", "http://localhost:8080/AccountSETemplate/api/account/createAccount", newObject).then(req => {
         console.log(req.responseText);
         let test = document.createElement("p");
         test.innerText = req.responseText;
-        document.getElementById("display2").appendChild(test);
-    }
-    req.open("POST", "http://localhost:8080/AccountSETemplate/api/account/createAccount");
-    req.send(newJSON);
+        document.getElementById("display").appendChild(test);
+    })
+
 }
 
 
 function deleteAnAccount() {
-    let req = new XMLHttpRequest();
-
     let x = document.getElementById("num1").value;
-    
-    req.onload = function () {
+    makeRequest("DELETE", "http://localhost:8080/AccountSETemplate/api/account/deleteAccount/" + x).then(req => {
         let test = document.createElement("p");
         test.innerText = req.responseText;
-        document.getElementById("display3").appendChild(test);
-    }
-    req.open("DELETE", "http://localhost:8080/AccountSETemplate/api/account/deleteAccount/" + x);
-    req.send();
+        document.getElementById("display").appendChild(test);
+    })
+    
 }
 
 function updateAnAccount() {
-    let req = new XMLHttpRequest();
 
+    let accName = document.getElementById("accName").value;
+    let firstName = document.getElementById("firstName").value;
+    let lastName = document.getElementById("lastName").value;
     let newObject = makeObject(accName, firstName, lastName);
-    let newJSON = JSON.stringify(newObject);
+
     let x = document.getElementById("num1").value;
 
-    req.onload = function () {
+    makeRequest("PUT", "http://localhost:8080/AccountSETemplate/api/account/updateAccount/" + x, newObject).then(req => {
         let test = document.createElement("p");
         test.innerText = req.responseText;
-        document.getElementById("display4").appendChild(test);
-    }
-    req.open("PUT", "http://localhost:8080/AccountSETemplate/api/account/updateAccount/" + x);
-    req.send(newJSON);
+        document.getElementById("display").appendChild(test);
+    })
+    
+}
+
+let aCondition = true;
+
+
+function makeRequest(method, url, body) {
+    return new Promise((resolve, reject) => {
+        const req = new XMLHttpRequest();
+
+        req.onload = () => {
+            if(req.status == 200){
+                resolve(req);
+            } else {
+                const reason = new Error("Rejected");
+                reject(reason);
+            }
+        };
+        req.open(method, url);
+        req.send(body);
+    });
 }
 
 
 
+
+
+//const consumePromise = function () {
+//    makePromise().then((user) => { console.log(user); }).catch(function (error) { console.log(error.message); });
+//};
+
+//consumePromise();
